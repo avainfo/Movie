2ed2eb9f357b376778bdf86e4eb3d7cbb82b992c
chahine <chahine.ouasti@gmail.com>
2024-06-06 13:34:47 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'narutoo',
+  webDir: 'www'
+};
+
+export default config;
